ed2f157bc95024e75f718250c4f31f69ce80b6f3
Alberta McAleavey <a.mcaleavey24@gmail.com>
2022-02-10 15:52:45 -0500

    create board outline with grid

---
+++ js/app.js
@@ -0,0 +1,35 @@
+
+
+// 1. initialize the game
+  // set board to an array 
+  // display instructions for player to pick a category and price
+  // 
+
+// 2. render 
+
+
+  /*-------------------------------- Constants --------------------------------*/
+
+
+/*---------------------------- Variables (state) ----------------------------*/
+let win
+// if a game is in progress
+
+/*------------------------ Cached Element References ------------------------*/
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+/*-------------------------------- Functions --------------------------------*/
+
+// function init() {
+
+// }
+
+
+// function render() {
+//   board = [
+    
+//   ]
+// }
