b357844b553419f2f47f88f9b986f7a0333121ae
Alberta McAleavey <a.mcaleavey24@gmail.com>
2022-02-11 17:55:24 -0500

    update render

--- js/app.js
+++ js/app.js
@@ -18,6 +18,7 @@ let game
 let boardSpots = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
 let currentQuestion
 let clickedIdx
+let playerAns
 
 /*------------------------ Cached Element References ------------------------*/
 let boxes = document.querySelectorAll('.box')
@@ -25,8 +26,7 @@ let board = document.getElementById('board')
 let message = document.getElementById('message')
 let card = document.querySelector('.card')
 let submitAns = document.getElementById("button-addon2")
-let input 
-let userGuess
+
 
 
 /*----------------------------- Event Listeners -----------------------------*/
@@ -38,32 +38,48 @@ init()
 
 function init() {
   game = null
+  currentQuestion = null
+  clickedIdx = null
+  playerAns = null
   boardSpots = [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,]
   render()
 }
 
-
-
-// display prices on boxes using board array
+// starting game render
+  // displays prices on boxes using board array
+  // displays instructions
 
 function render() {
+
   // updates the board to display which boxes have already been picked 
+  
+  // displays message based on status of game
+  // if all squares are null, show instructions 
+  // if(boardSpots.every(function(spot){
+  //   spot === null
+  // })) {
+  //   message.innerText = "Instructions"
+  // }
+
+  // if(game === null) {
+  //   message.innerText = "instrtuctions"
+  // }
   boardSpots.forEach(function(el, idx){
     if(el !== null) {
          boxes[idx].innerText = ""
     } 
   })
-  // displays message based on status of game
-  message.innerText = userGuess
-  // if(game === null){
-  //   message.innerText = "Pick Again "
-  // } else if (game === "over") {
-  //   message.innerText = "Game Over"
-  // } else if (game === "won") {
-  //   message.innerText = "You won!"
-  // }
+  card.style.display = "none"
+  if(playerAns === "correct") {
+    message.innerText = "Correct! Pick again"
+  } else if (playerAns === "incorrect") {
+    message.innerText = "Incorrect, pick again"
+  }
 
 }
+
+
+
 
 function handleBoxClick(evt) {
   // find the id of the element that was clicked 
@@ -84,21 +100,20 @@ function handleBoxClick(evt) {
 }
 
 function handleSubmitAns() {
-  input = document.querySelector('.form-control').value.toLowerCase()
+let input = document.querySelector('.form-control').value.toLowerCase()
 console.log(input)
+// checks if input is correct and updates playerGuess variable accordingly
+if(input === answers[clickedIdx]){
+    playerAns = "correct"
+  } else {
+    playerAns= "incorrect"
+  }
   
-  checkAns()
   render()
 }
 
-function checkAns() {
-  if(input === answers[clickedIdx]){
-    userGuess = "correct"
-  } else {
-    userGuess = "incorrect"
-  }
-}
 
+  
 
 
 
@@ -119,3 +134,8 @@ function checkAns() {
 // })){
 //   ongoingGame === false
 // }
+
+
+// 1. show instructions
+// 2. after first pick, while game is ongoing, show if answer was right or wrong and to pick again
+// 3. if game is over, (all squares empty/clicked), show if player won or lost and to play again
