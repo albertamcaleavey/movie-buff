2ce0add0498b34a2cd282e73a8e50321b63a221a
Alberta McAleavey <a.mcaleavey24@gmail.com>
2022-02-11 10:41:15 -0500

    add second class name to boxes and wrote confitional in render function

--- js/app.js
+++ js/app.js
@@ -14,41 +14,46 @@
 /*---------------------------- Variables (state) ----------------------------*/
 let win
 // if a game is in progress
-let board = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
+let boardSpots = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
 
 /*------------------------ Cached Element References ------------------------*/
 let boxes = document.querySelectorAll('.box')
 let message = document.getElementById('message')
-console.log(message)
+// console.log(boxes)
+
 
 /*----------------------------- Event Listeners -----------------------------*/
 
 
 /*-------------------------------- Functions --------------------------------*/
+init()
 
 function init() {
   win = null
-  board = [null, null, null, null, null, null, null, null, null, null,null, null, null, null, null, null, null, null, null, null, null, null, null, null, null]
-  render()
+  boardSpots = [null, 1, null, 2, null, 5, null, null, null, null,null, null, null, null, null, null, null, null, null, null, null, null, null, null, null]
+  renderBoard()
 }
 
 
 // display prices on boxes using board array
 
-function render() {
-  board = [ "$200", "$200", "$200", "$200", "$200", "$400", "$400", "$400", "$400", "$400", "$600", "$600", "$600", "$600", "$600", "$800", "$800", "$800", "$800", "$800", "$1000", "$1000", "$1000", "$1000", "$1000"  
-  ]
+function renderBoard() {
+  boardSpots.forEach(function(el, idx){
+    if(el !== null) {
+         boxes[idx].innerText = ""
+    } 
+  })
 }
 
 
 // display a question based on which box was clicked
-function renderQuestion() {
+// function renderQuestion() {
 
-}
+// }
 
 // check if a game is in progress
   // game is over when all boxes are empty
   // player wins if they make over a certain number of points
-function ongoingGame() {
+// function ongoingGame() {
 
-} 
+// } 
