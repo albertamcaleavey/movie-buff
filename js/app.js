7b09c8c62e0f40901875a2a61f04856cf69b4e3b
Alberta McAleavey <a.mcaleavey24@gmail.com>
2022-02-11 21:59:46 -0500

    update functions and box text

--- js/app.js
+++ js/app.js
@@ -1,24 +1,18 @@
 
-
-// 1. initialize the game
-  // set board to an array 
-  // display instructions for player to pick a category and price
-  // 
-
-// 2. render 
-
-
   /*-------------------------------- Constants --------------------------------*/
 import{questions, answers} from "../data/questions&answers.js"
 
+const winScore = 7500
+// half of possible points
+// to win, score must be greate than 7500
 /*---------------------------- Variables (state) ----------------------------*/
-// if the player won
 let game
 // checking if game is in progress, game over, or game won
 let boardSpots = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
 let currentQuestion
 let clickedIdx
 let playerAns
+let score
 
 /*------------------------ Cached Element References ------------------------*/
 let boxes = document.querySelectorAll('.box')
@@ -41,6 +35,7 @@ function init() {
   currentQuestion = null
   clickedIdx = null
   playerAns = null
+  score = 0
   boardSpots = [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,]
   render()
 }
@@ -59,9 +54,9 @@ boardSpots.forEach(function(el, idx){
   // displays message based on status of game
   if(game === null){
     message.innerText = "instructions"
-  } else if (game === "over") {
-    message.innerText = "Game Over"
-  } else if (game === "won") {
+  } else if (game === "loss") {
+    message.innerText = "Game Over, you lost"
+  } else if (game === "win") {
     message.innerText = "You won!"
   }
   // if all squares are null, show instructions 
@@ -86,9 +81,8 @@ function handleBoxClick(evt) {
   // separates the number from the rest of the id
   // turns the id into a number with parseInt
   clickedIdx = parseInt(clickedBoxId.split('').splice(3,3).join(''))
-  boardSpots[clickedIdx] = ""
-  // find the corresponding question in the questions array
-  // update the index of the board array 
+
+  // find the corresponding question
   currentQuestion = questions[clickedIdx]
   // set the current question text to the card
   card.innerText = currentQuestion
@@ -96,29 +90,43 @@ function handleBoxClick(evt) {
   card.style.display = "inline"
   // call render function to display the question
   render()
-  // check if there's a win
-  // checkWin()
 }
+
 
 //---------------------WHEN-ANSWER-IS-SUBMITED----------------------//
 
 function handleSubmitAns() {
+// get the user input and makes it all lowercase
 let input = document.querySelector('.form-control').value.toLowerCase()
-console.log(input)
-// checks if input is correct and updates playerGuess variable accordingly
+
+// SHOULD THIS GO IN A CHECK ANSWER FUNCTION???
+// checks if input is correct: updates playerAns variable and score
 if(input === answers[clickedIdx]){
     playerAns = "correct"
+    score += parseInt(boxes[clickedIdx].textContent)
+    console.log(score)
   } else {
     playerAns= "incorrect"
   }
-  // SHOULD THE BELOW GO IN A SECOND RENDER FUNCTION???
+// update the index of the board array 
+boardSpots[clickedIdx] = ""
+  // SHOULD THIS GO IN A SECOND RENDER FUNCTION???
   card.style.display = "none"
   render()
+  // check if there's a win
+  checkWin()
 }
 
-
+//-----------------------CHECK-STATUS-OF-GAME-----------------------//
   
-function
+function checkWin(){
+// if all squares are empty, game is over=> check if player has won depending on if score is greater than a certain number 
+
+// if score is greater than winning score, update game variable to win in order to trigger render win message (extra: display how many questions they got right)
+
+// if score is less than winning score, update game variable to loss to render message 
+
+}
 
 
 // RENDER FLOW
