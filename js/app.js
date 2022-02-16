7f08c9e2d5b44fe29f452e1219fc2261e801d78a
Alberta McAleavey <a.mcaleavey24@gmail.com>
2022-02-16 00:19:02 -0500

    change scoreboard image and update input style

--- js/app.js
+++ js/app.js
@@ -1,16 +1,14 @@
 
   /*-------------------------------- Constants --------------------------------*/
 import{questions, answers} from "../data/questions&answers.js"
-
 const winScore = 7500
-// half of possible points
 
 const correctAudio = new Audio ("../audio/correct.wav")
 const incorrectAudio = new Audio ("../audio/incorrect.mp3")
 
 /*---------------------------- Variables (state) ----------------------------*/
 let game
-let boardSpots = []
+let boardSpots 
 let clickedIdx
 let playerAns
 let score
@@ -20,18 +18,15 @@ let timeLeft
 /*------------------------ Cached Element References ------------------------*/
 let boxes = document.querySelectorAll('.box')
 let board = document.getElementById('board')
-// having boxes and board may be repetitive 
+
+// having boxes and board is repetitive 
+// change event listener to listen for boxes instead of board
 let message = document.getElementById('message')
 let progressBar = document.querySelector('.progress')
-
-
 let replayBtn = document.querySelector(".btn-primary")
-let scoreboard = document.getElementById("score")
-
-// GROUP THESE??
-let card = document.querySelector(".background")
-let timerEl = document.getElementById("timer")
-// let form = document.querySelector(".input")
+let scoreboard = document.getElementById("scoreboard")
+let questionCard = document.querySelector(".background")
+let input = document.getElementById('input')
 
 
 /*----------------------------- Event Listeners -----------------------------*/
@@ -47,15 +42,14 @@ function init() {
   clickedIdx = null
   playerAns = null
   score = 0
+  timer = null
   timeLeft = 30
   boardSpots = [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,]
-
-  card.style.display = "none"
-  // form.style.display = "none"
+  // hide question card, progress bar and scoreboard
+  questionCard.style.display = "none"
   progressBar.style.display = "none"
-
   scoreboard.style.display = "none"
-  //assign prices to divs
+  // display box prices 
   document.querySelectorAll(".two-hun").forEach((el) => el.innerText = "200")
   document.querySelectorAll(".four-hun").forEach((el) => el.innerText = "400")
   document.querySelectorAll(".six-hun").forEach((el) => el.innerText = "600")
@@ -65,16 +59,16 @@ function init() {
 }
 
 // ----------------------RENDER-GAME-STATUS-----------------------//
-// starting game render
-  // displays prices on boxes using board array
+
+  // displays prices on boxes
   // displays instructions
 
 function render() {
   // displays message based on status of game
   if(game === null){
-    message.innerText = "Select a question. If you get more than half the questions right, you win!"
+    message.innerText = "Select a question. If you earn over 7500 points, you win!"
   } else if (game === "loss") {
-    message.innerText = "Game Over, you lost."
+    message.innerText = "Game Over, you lost. Click the replay button to try again!"
   } else if (game === "win") {
     message.innerText = "You won!"
   }
@@ -85,44 +79,40 @@ function render() {
 
 function handleBoxClick(evt) {
   let clickedBox = evt.target
-  // if a category box is clicked, nothing happens
-  if(clickedBox.className === "cat"){
+  // if a category box or a box that was already chosen is clicked, nothing happens
+  if(clickedBox.className === "cat" || clickedBox.innerText === ""){
     return
   }
   // find the id of the element that was clicked 
   let clickedBoxId = clickedBox.id
-  // separates the number from the rest of the id
-  // turns the id into a number with parseInt
+  // separates the number from the rest of the id and turns it into a number
   clickedIdx = parseInt(clickedBoxId.split('').splice(3,3).join(''))
-  renderQuestion()
+  renderQuestionCard()
   startTimer()
 
 }
 
 // ---------------------------TIMER----------------------------//
-// may need to init timeleft to zero
+
 function startTimer() {
+  // create cached element reference for progress bar
   let progress = document.querySelector(".progress-bar")
   timer = setInterval(function() {
-  timerEl.textContent = timeLeft + " seconds remaining!"
+  // decreases time by one second
   timeLeft -= 1
-  //  let timePassed = 30 - timeLeft
+  // adjusts the width of progress bar to time left
   progress.style =`width: ${timeLeft/30 *100}%`
   progress.innerText = timeLeft
 
   if (timeLeft < 0) {
-    timerEl.textContent = "Time's Up!" 
     incorrectAudio.play()
     clearInterval(timer)
     checkAnswer()
   }
-  console.log(timeLeft)
   }, 1000)
   }
 
 function resetTimer() {
-  timerEl.textContent = ""
-  // progressBar.style =`width: 0%`
   clearInterval(timer)
   timeLeft = 30
   timer = null
@@ -131,49 +121,42 @@ function resetTimer() {
 
 //-----------------------DISPLAYS-QUESTION----------------------//
 
-function renderQuestion() {
+function renderQuestionCard() {
   // find the corresponding question
   let currentQuestion = questions[clickedIdx]
   // set the current question text to the card
   document.getElementById("question").innerText = currentQuestion
-  // show the card- should this go in render?
-  card.style.display = "grid"
-  // show the input form
-  // form.style.display = "grid"
-  // show timer
+  // display question card
+  questionCard.style.display = "grid"
+  // display timer
   progressBar.style.display = "grid"
 }
 
 
 //---------------------WHEN-ANSWER-IS-SUBMITED----------------------//
 
-// change name to check answer??
 function checkAnswer() {
-  // get the user input and makes it all lowercase
-  let input = document.querySelector('.input').value.toLowerCase()
+  // retrieve user input and make it lowercase
+  let lowerInput = input.value.toLowerCase()
   // checks if input is correct: updates playerAns variable and score
-if(input === answers[clickedIdx]){
+  if(lowerInput === answers[clickedIdx]){
     playerAns = "correct"
     score += parseInt(boxes[clickedIdx].textContent)
   } else {
     playerAns= "incorrect"
-    // call a render correct answer function??
+    // create and call a render correct answer function??
   }
-  // make an option for when there is no response and timer runs out
 
-  // SHOULD THIS GO IN A RENDER FUNCTION??
-// update the index of the board array 
-boardSpots[clickedIdx] = ""
-// clear input field 
-document.querySelector('.input').value = ""
+  // SHOULD THIS GO IN ANOTHER RENDER FUNCTION??
+  // update the index of the board array 
+  boardSpots[clickedIdx] = ""
+  // clear input field 
+  input.value = ""
 
-// hide question card
-card.style.display = "none"
-// hide input form
-// form.style.display = "none"
-// hide timer/ progress bar
-progressBar.style.display = "none"
-
+  // hide question card
+  questionCard.style.display = "none"
+  // hide progress bar
+  progressBar.style.display = "none"
   renderResult()
   // check if there's a win
   checkGameStatus()
@@ -190,7 +173,7 @@ function renderResult() {
          boxes[idx].innerText = ""
     } 
   })
-  // shows a message stating whether the answer was right or wrong 
+  // renders message for right or wrong answer 
   if(playerAns === "correct") {
     message.innerText = "Correct! Pick again"
     // show score board with updated score if answer is right
@@ -225,13 +208,11 @@ function playIncorrectAudio() {
 
 //-----------------------CHECK-STATUS-OF-GAME-----------------------//
   
-// change to checkGameStatus????
 function checkGameStatus(){
 // checks if game is over
 let checkBoard = boardSpots.every(function(spot) {
   return spot !== null
 })
-console.log(checkBoard)
 // if the game is over, check for win/ loss
 if(checkBoard=== true){
   checkWin()
